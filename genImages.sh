27b0737a0ba260bd700675fca4651bd912bf09c1
chuanzhubin <chuanzhubin_zh@chinamoney.com.cn>
2018-02-07 17:07:51 +0800

    2

--- genImages.sh
+++ genImages.sh
@@ -2,7 +2,7 @@
 
 DMALL_DOCKER_REGISTRY="ec2-54-95-48-23.ap-northeast-1.compute.amazonaws.com:5000"
 SLUG=chuanzhubin
-BUILD_NUMBER=1
+BUILD_NUMBER=2
 sudo docker build -t $DMALL_DOCKER_REGISTRY/$SLUG/inventory-service:$BUILD_NUMBER .
 sudo docker tag $DMALL_DOCKER_REGISTRY/$SLUG/inventory-service:$BUILD_NUMBER $DMALL_DOCKER_REGISTRY/$SLUG/inventory-service:latest
 
